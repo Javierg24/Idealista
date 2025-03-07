8605b0b7264534bff1c2ad7573bc23a88f567f58
Nandomata11 <163398943+Nandomata11@users.noreply.github.com>
2025-03-07 13:17:22 +0100

    cambios para traerse las imagenes

--- src/app/card-inmueble/card-inmueble.component.ts
+++ src/app/card-inmueble/card-inmueble.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ImagenesService } from '../services/imagenes.service';
 
 @Component({
   selector: 'app-card-inmueble',
@@ -7,4 +8,22 @@ import { Component, Input } from '@angular/core';
 })
 export class CardInmuebleComponent {
   @Input() datos: any;
+  rutaImagen: string = 'http://localhost/idealista/IMAGES/noFoto.jpg';
+
+  constructor(private imageService: ImagenesService){}
+
+  ngOnInit(): void {
+    this.getImagen(this.datos.id_propiedad);
+  }
+
+  getImagen(idPropiedad: number) {
+    this.imageService.obtenerImagenesDePropiedad(idPropiedad).subscribe(
+      (data: any) => {
+        this.rutaImagen = data[0].url_imagen;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
